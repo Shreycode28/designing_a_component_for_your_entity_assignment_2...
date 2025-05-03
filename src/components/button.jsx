af4a59c6e0af5e1cd56a176a4793267535435b8c
Shreycode28 <shrey.patel.s72@kalvium.community>
2025-05-03 08:54:20 +0530

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,11 @@
-// write button card here
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+      View Product
+    </button>
+  );
+};
+
+export default Button;
